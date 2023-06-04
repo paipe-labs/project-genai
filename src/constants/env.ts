d6b20b4736388fe30f62723888d7873969e96b6d
Savelii Novikov <kvaka2000@gmail.com>
2023-06-04 20:43:27 +0200

    Yet another updated HTTP port

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,5 +1,5 @@
 import { tryParseInt } from "utils/parse"
 
-export const HTTP_PORT = tryParseInt(process.env.HTTP_PORT ?? '', 10, 40);
+export const HTTP_PORT = tryParseInt(process.env.HTTP_PORT ?? '', 10, 8081);
 export const WS_PORT = tryParseInt(process.env.WS_PORT ?? '', 10, 8080);
 export const JWT_SECRET = process.env.JWT_SECRET ?? '';
