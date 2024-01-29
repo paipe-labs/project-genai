59fc85d5bc394c37dbcd8ecb0f2a983a16e9251f
Daria Onikova <onikovad@gmail.com>
2024-01-29 19:46:55 +0000

    rm extra .ts file

--- backend-python/src/utils/delay.ts
+++
@@ -1,2 +0,0 @@
-export const delay = async (delayMs: number): Promise<void> =>
-  new Promise((resolve) => setTimeout(resolve, delayMs));
--- backend-python/src/utils/jtw.ts
+++
@@ -1,6 +0,0 @@
-import { JWT_SECRET } from 'constants/env'
-import jwt from 'jsonwebtoken'
-
-export const jwtVerify = (accessToken: string) => {
-  return jwt.verify(accessToken, JWT_SECRET, { complete: false })
-}
