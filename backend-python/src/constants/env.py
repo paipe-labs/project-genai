f21799413d29cc09341b0913f5de3281eeead625
Alexey Penkov <vvauijij@gmail.com>
2024-02-27 00:02:51 +0300

    schema_validation

--- backend-python/src/constants/env.py
+++ backend-python/src/constants/env.py
@@ -1,7 +1,10 @@
 import os
+
 from utils.parse import parse
 
-HTTP_WS_PORT = parse(os.environ.get("HTTP_PORT", ""), 8080, int)
-ENFORCE_JWT_AUTH = parse(os.environ.get("ENFORCE_JWT_AUTH", ""), True, bool)
-JWT_SECRET = os.environ.get("JWT_SECRET", "")
 DEBUG = parse(os.environ.get("DEBUG", ""), False, bool)
+ENFORCE_JWT_AUTH = parse(os.environ.get("ENFORCE_JWT_AUTH", ""), True, bool)
+HTTP_WS_PORT = parse(os.environ.get("HTTP_PORT", ""), 8080, int)
+JWT_SECRET = os.environ.get("JWT_SECRET", "")
+TASK_RESULT_SCHEMA_PATH = os.environ.get("TASK_RESULT_SCHEMA_PATH", "src/task_result_schema.json")
+TASK_SCHEMA_PATH = os.environ.get("TASK_SCHEMA_PATH", "src/task_schema.json")
--- backend-python/src/ws_connection.py
+++ backend-python/src/ws_connection.py
@@ -1,7 +1,14 @@
+import websocket
+import json
+import jsonschema
+
+from constants.env import TASK_RESULT_SCHEMA_PATH, TASK_SCHEMA_PATH
 from dispatcher.network_connection import NetworkConnection
 from dispatcher.task import Task
 import websocket
 import json
+from dispatcher.util.logger import logger
+from dataclasses import asdict
 
 
 class WSConnection(NetworkConnection):
@@ -9,34 +16,56 @@ class WSConnection(NetworkConnection):
         super().__init__()
         self.ws = ws
 
+        with open(TASK_SCHEMA_PATH) as task_schema:
+            self.task_schema = json.load(task_schema)
+        with open(TASK_RESULT_SCHEMA_PATH) as task_result_schema:
+            self.task_result_schema = json.load(task_result_schema)
+
     def restoreConnection(self, ws: websocket.WebSocket):
         self.ws = ws
         self.onConnectionRestored()
 
     def send_task(self, task: Task):
-        clientTask = {'taskId': task.id}
+        clientTask = {"taskId": task.id}
 
-        if task.task_options.standard_pipeline:
-            clientTask['options'] = {
-                'prompt': task.task_options.standard_pipeline.prompt,
-                'model': task.task_options.standard_pipeline.model,
-                'size': task.task_options.standard_pipeline.size,
-                'steps': task.task_options.standard_pipeline.steps,
+        clientTask["options"] = (
+            {
+                "prompt": task.task_options.standard_pipeline.prompt,
+                "model": task.task_options.standard_pipeline.model,
+                "size": task.task_options.standard_pipeline.size,
+                "steps": task.task_options.standard_pipeline.steps,
             }
+            if task.task_options.standard_pipeline
+            else None
+        )
 
-        if task.task_options.comfy_pipeline:
-            clientTask['comfyOptions'] = {
-                'pipelineData': task.task_options.comfy_pipeline.pipeline_data,
-                'pipelineDependencies': task.task_options.comfy_pipeline.pipeline_dependencies,
+        clientTask["comfyOptions"] = (
+            {
+                "pipelineData": task.task_options.comfy_pipeline.pipeline_data,
+                "pipelineDependencies": task.task_options.comfy_pipeline.pipeline_dependencies,
             }
-        self.ws.send(json.dumps(clientTask))
+            if task.task_options.comfy_pipeline
+            else None
+        )
+
+        try:
+            jsonschema.validate(instance=clientTask, schema=self.task_schema)
+            self.ws.send(json.dumps(clientTask))
+        except Exception as e:
+            logger.warn(
+                f"Task {task.id} was not sent due to schema validation error: {e}"
+            )
 
     def abortTask(self, task: Task):
-        self.ws.send(
-            json.dumps(
-                {
-                    "type": "abort",
-                    "task_id": task.id,
-                }
+        clientTaskAbort = {
+            "type": "abort",
+            "taskId": task.id,
+        }
+
+        try:
+            jsonschema.validate(instance=clientTaskAbort, schema=self.task_schema)
+            self.ws.send(json.dumps(clientTaskAbort))
+        except Exception as e:
+            logger.warn(
+                f"Task {task.id} was not aborted due to schema validation error: {e}"
             )
-        )
