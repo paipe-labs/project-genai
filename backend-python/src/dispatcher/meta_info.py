43ede9dbeb67bea17353cfa0b8cd74a81304250c
Nina Gulevich <nina.m.gulevich@gmail.com>
2024-03-31 12:18:49 +0300

    client-node update and adapt to old version of 'register'

--- backend-python/src/dispatcher/meta_info.py
+++ backend-python/src/dispatcher/meta_info.py
@@ -1,12 +1,13 @@
 import typing
 
+
 class PublicMetaInfo(typing.NamedTuple):
     models: list[str]
     gpu_type: str
     ncpu: int
     ram: int
 
-    min_cost: int # TODO: get rid of this
+    min_cost: int  # TODO: get rid of this
 
 
 class PrivateMetaInfo(typing.NamedTuple):
--- backend-python/src/run.py
+++ backend-python/src/run.py
@@ -28,8 +28,12 @@ import uuid
 monkey.patch_all()
 
 app = Flask(__name__)
-CORS(app, allow_headers="*", origins="*",
-     methods="GET, POST, PATCH, PUT, DELETE, OPTIONS")
+CORS(
+    app,
+    allow_headers="*",
+    origins="*",
+    methods="GET, POST, PATCH, PUT, DELETE, OPTIONS",
+)
 sock = Sock(app)
 
 entry_queue = EntryQueue()
@@ -290,12 +294,14 @@ def websocket_connection(ws):
             msg_type = data_json.get("type")
             if msg_type == "register":
                 node_id = data_json.get("node_id")
-                metadata = data_json.get("metadata")
-                public_meta = PublicMetaInfo(models=metadata.get("models", []),
-                                             gpu_type=metadata.get("gpu_type", ""),
-                                             ncpu=metadata.get("ncpu", 0),
-                                             ram=metadata.get("ram", 0),
-                                             min_cost=10)
+                metadata = data_json.get("metadata", dict())
+                public_meta = PublicMetaInfo(
+                    models=metadata.get("models", []),
+                    gpu_type=metadata.get("gpu_type", ""),
+                    ncpu=metadata.get("ncpu", 0),
+                    ram=metadata.get("ram", 0),
+                    min_cost=10,
+                )
                 print(f"Node {node_id} connected")
                 if node_id in dispatcher.providers_map:
                     existing_provider = dispatcher.providers_map[node_id]
